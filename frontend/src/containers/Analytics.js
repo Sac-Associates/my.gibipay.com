8542db2a9689e6498a4df18ec98f5ebb6e6d6801
motart <abadlirachid@gmail.com>
2024-09-20 22:39:42 -0700

    lost changes

---
+++ frontend/src/containers/Analytics.js
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Analyics () {
+    return (
+        <>
+            <Box>
+                <Typography>
+                    Analyics
+                </Typography>
+            </Box>
+        </>
+    )
+}
